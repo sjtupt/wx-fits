34abc1fc9a55c04c3c458c36457ff4e6b138c6a7
peitao <peitao@wanda.cn>
2017-04-19 15:22:11 +0800

    function develop

--- src/pages/record/record.js
+++ src/pages/record/record.js
@@ -1,9 +1,12 @@
 import * as Storage from '../../utils/storage'
+var util = require('../../utils/util.js')
 
 // 获取全局应用程序实例对象
 // const app = getApp()
 
 const timeNoons = [{id: 0, name: '上午', timePeriod:'03:00am - 09:00am'},{id: 1, name: '中午', timePeriod:'09:00am - 15:00pm'},{id: 2, name: '下午', timePeriod:'15:00pm - 21:00pm'},{id: 3, name: '晚上', timePeriod:'21:00pm - 03:00pm'}]
+
+const validWeight = input => input.match(/^(\d+)(\.?)(\d*)$/g) == null ? false : true
 
 // 创建页面实例对象
 Page({
@@ -11,12 +14,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-    title: '记一记'
+    title: '记一记',
+    recordBtnDisabled: true
   },
-  //
-  // bindinput(e){
-  //   console.log(e.detail.value)
-  // },
+
+  bindinput(e) {
+
+    let inputString = e.detail.value
+    if (!validWeight(inputString)) {
+      wx.showToast({
+        title: '亲，输入有误',
+        icon: 'warn',
+        duration: 2000
+      })
+
+      return inputString.substring(0, inputString.length-1)
+    }
+
+    if (parseInt(inputString) > 0 && parseInt(inputString) < 150) {
+      this.setData({
+        recordBtnDisabled: false,
+        currentValidWeight: parseInt(inputString)
+      })
+    } else {
+      this.setData({
+        recordBtnDisabled: true
+      })
+    }
+  },
+
   chooseDay() {
     wx.navigateTo({
       url: '../selectDay/selectDay'
@@ -25,25 +51,32 @@ Page({
   chooseTime() {
     wx.showActionSheet({
       itemList: timeNoons.map((item) => item.timePeriod),
-      success: function(res) {
-        console.log(res.tapIndex)
-      },
+      success: res => this.setCurrentTimeIndex(res.tapIndex),
       fail: function(res) {
         console.log(res.errMsg)
       }
     })
   },
-  save(weight) {
-    Storage.getLocalDataByKey(Storage.kWeightInfo).then(res => {
-      console.log(res)
-    }, err => {
-      console.log(err)
-    })
+  save() {
+    // Storage.getLocalDataByKey(Storage.kWeightInfo).then(res => {
+    //   console.log(res)
+    // }, err => {
+    //   console.log(err)
+    // })
 
     Storage.setLocalData(Storage.kWeightInfo)({
-      weight: weight,
+      weight: this.data.currentValidWeight,
       time: Date.now()
-    }).then(res => console.log('set suc', res), err => console.log('err', err))
+    }).then(
+      res => {
+        wx.switchTab({
+          url: '/pages/home/home'
+        })
+      },
+      err => {
+        console.log('err', err)
+      })
+
   },
   bindblur(e){
     // this.save(e.detail.value)
@@ -52,46 +85,27 @@ Page({
     this.save(e.detail.value)
   },
 
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad () {
-    // TODO: onLoad
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady () {
-    // TODO: onReady
+  setCurrentTimeIndex(index) {
+    if (index < 0 || index >= timeNoons.length) {
+      return
+    }
+    this.setData({
+      currentTimeIndex: index,
+      currentTimeShow: timeNoons[index]
+    })
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow () {
-    // TODO: onShow
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide () {
-    // TODO: onHide
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload () {
-    // TODO: onUnload
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh () {
-    // TODO: onPullDownRefresh
+    this.setCurrentTimeIndex(0)
+    this.setData({
+      recordBtnDisabled: true,
+      currentValidWeight: 0,
+      inputValue: null,
+      dayShow: util.dateFormat(new Date(), 'yyyy-MM-dd'),
+      dayDetailShow: util.relationWithToday(new Date())
+    })
   }
 })
--- src/utils/util.js
+++ src/utils/util.js
@@ -15,6 +15,61 @@ function formatNumber (n) {
   return n[1] ? n : '0' + n
 }
 
+function dateFormat(date, format) {
+  // if (typeof date !== 'object') {
+  //   date = new Date(date.replace(/-/g, '/'));
+  // }
+  const map = {
+    'M': date.getMonth() + 1, //月份
+    'd': date.getDate(), //日
+    'h': date.getHours(), //小时
+    'm': date.getMinutes(), //分
+    's': date.getSeconds(), //秒
+    'q': Math.floor((date.getMonth() + 3) / 3), //季度
+    'S': date.getMilliseconds() //毫秒
+  };
+  format = format.replace(/([yMdhmsqS])+/g, function(all, tNum) {
+    let vNum = map[tNum];
+    if (vNum !== undefined) {
+      if (all.length > 1) {
+        vNum = '0' + vNum;
+        vNum = vNum.substr(vNum.length - 2);
+      }
+      return vNum;
+    } else if (tNum === 'y') {
+      return (date.getFullYear() + '').substr(4 - all.length);
+    }
+    return all;
+  });
+  return format;
+}
+
+const oneDay = 24 * 60 * 60 * 1000
+
+function relationWithToday(date) {
+  let now = new Date()
+  let nowWithoutHours = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+  let dateWithoutHours = new Date(date.getFullYear(), date.getMonth(), date.getDate())
+
+  let timeSpan = nowWithoutHours.valueOf() < dateWithoutHours.valueOf()
+  if (timeSpan < 0) {
+    return ''
+  } else if (timeSpan == 0) {
+    return '今天'
+  } else {
+    let days = timeSpan / oneDay
+    if (days > 365) {
+      return days/365 + '年前'
+    } else if (days > 30) {
+      return days/30 + '月前'
+    } else {
+      return days + "天前"
+    }
+  }
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  dateFormat: dateFormat,
+  relationWithToday: relationWithToday
 }
